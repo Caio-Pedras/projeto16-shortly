b53c10f18b1958373844348e31925d811f378801
CaioPedras <caio.vp@gmail.com>
2022-08-02 18:45:29 -0300

    Signup + Signin

--- routes/sessionRouter.js
+++ routes/sessionRouter.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
+import { validateSchema } from "../controllers/schemaValidator.js";
+import { signIn, signUp } from "../controllers/sessionController.js";
+import userSchema from "../schemas/userSchema.js";
+import signInSchema from "../schemas/signInSchema.js";
 
 const sessionRouter = Router();
 
-sessionRouter.post("/signup");
-sessionRouter.post("/signin");
+sessionRouter.post("/signup", validateSchema(userSchema), signUp);
+sessionRouter.post("/signin", validateSchema(signInSchema), signIn);
 
 export default sessionRouter;
