27fb166ad7101c9c605777d9c43268aa98eb69fe
CaioPedras <caio.vp@gmail.com>
2022-08-03 16:10:53 -0300

    fix typo

--- repositories/urlRepository.js
+++ repositories/urlRepository.js
@@ -2,12 +2,14 @@ import db from "../config/db.js";
 
 async function insertShortUrl(userId, url, shortUrl) {
   return db.query(
-    `INSERT INTO sessions ( "userId", url, "shortUrl") 
+    `INSERT INTO urls ( "userId", url, "shortUrl") 
     VALUES ($1, $2, $3)`,
     [userId, url, shortUrl]
   );
 }
-
+async function selectUrlById(id) {
+  return db.query(`SELECT * FROM `);
+}
 export const urlRepository = {
   insertShortUrl,
 };
