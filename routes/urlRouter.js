bb1073ecf0ac1d55da94d60bcf36166e620fd0c6
CaioPedras <caio.vp@gmail.com>
2022-08-03 16:08:56 -0300

    esboço

--- routes/urlRouter.js
+++ routes/urlRouter.js
@@ -1,10 +1,24 @@
 import { Router } from "express";
+import { validateSchema } from "../middlewares/schemaValidator.js";
+import {
+  deleteUrlById,
+  getUrlById,
+  openUrl,
+  reduceUrl,
+} from "../controllers/urlController.js";
+import urlSchema from "../schemas/urlSchema.js";
+import { validateToken } from "../middlewares/tokenValidator.js";
 
 const urlRouter = Router();
 
-urlRouter.post("/urls/shorten");
-urlRouter.get("/urls/:id");
-urlRouter.get("/urls/open/:shortUrl");
-urlRouter.delete("/urls/:id");
+urlRouter.post(
+  "/urls/shorten",
+  validateSchema(urlSchema),
+  validateToken,
+  reduceUrl
+);
+urlRouter.get("/urls/:id", getUrlById);
+urlRouter.get("/urls/open/:shortUrl", openUrl);
+urlRouter.delete("/urls/:id", validateToken, deleteUrlById);
 
 export default urlRouter;
