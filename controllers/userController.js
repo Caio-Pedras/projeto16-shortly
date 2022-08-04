8768934798112741432fa0ddfb6d2efd793276f2
CaioPedras <caio.vp@gmail.com>
2022-08-04 14:19:25 -0300

    uesrController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,18 @@
-// import { sessionRepository, userRepository } from "../repositories/index.js";
+import { urlRepository } from "../repositories/index.js";
 
 export async function getUserInfo(req, res) {
+  const { id, name } = res.locals.user;
   try {
+    const { rows: shortenedUrls } = await urlRepository.selectAllUrlByUser(id);
+    const { rows: visitCountResult } = await urlRepository.sumVisitCount(id);
+    const visitCount = visitCountResult[0].sum || 0;
+    const body = {
+      id,
+      name,
+      visitCount,
+      shortenedUrls,
+    };
+    res.status(200).send(body);
   } catch (err) {
     console.log(err);
     res.sendStatus(500);
@@ -9,6 +20,8 @@ export async function getUserInfo(req, res) {
 }
 export async function getRanking(req, res) {
   try {
+    const { rows: ranking } = await urlRepository.getUrlsRanking();
+    res.status(200).send(ranking);
   } catch (err) {
     console.log(err);
     res.sendStatus(500);
--- repositories/urlRepository.js
+++ repositories/urlRepository.js
@@ -24,11 +24,43 @@ async function updateVisitCount(id) {
 async function deleteUrlById(id) {
   return db.query(`DELETE FROM urls WHERE id=$1`, [id]);
 }
+async function selectAllUrlByUser(userId) {
+  return db.query(
+    `
+    SELECT urls.id AS id, urls."shortUrl" AS "shortUrl", urls.url AS url, urls."visitCount" AS "visitCount"  
+    FROM urls 
+    WHERE "userId"=$1`,
+    [userId]
+  );
+}
+async function sumVisitCount(userId) {
+  return db.query(
+    `
+    SELECT SUM("visitCount") 
+    FROM urls 
+    WHERE "userId" =$1`,
+    [userId]
+  );
+}
+async function getUrlsRanking() {
+  return db.query(
+    `SELECT users.id AS id, users.name AS name, COUNT(urls.id) AS "linksCount", SUM(urls."visitCount") AS "visitCount"
+    FROM urls
+    JOIN users ON urls."userId" = users.id
+    GROUP BY users.id
+    ORDER BY "visitCount" DESC
+    LIMIT 10
+   `
+  );
+}
 
 export const urlRepository = {
   insertShortUrl,
   selectUrlById,
-  deleteUrlById,
   selectUrlByShortUrl,
   updateVisitCount,
+  deleteUrlById,
+  selectAllUrlByUser,
+  sumVisitCount,
+  getUrlsRanking,
 };
